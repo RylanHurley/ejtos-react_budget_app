a9c4ece7b6968341356520fd4bec33e418ee11ef
Rylan Hurley <rylan6605@gmail.com>
2024-06-28 15:16:39 -0400

    changes made from ejtos-react_budget_appt

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
+import AllocationForm from './components/AllocationForm';
+import ExpenseItem from './components/ExpenseItem';
+import ExpenseList from './components/ExpenseList';
+import ExpenseTotal from './components/ExpenseTotal';
+import Remaining from './components/Remaining';
 import Budget from './components/Budget';
-
 // Add code to import the other components here under
 
 
@@ -16,27 +20,40 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {
-                            /* Add Budget component here */
+                           // Budget component
+                    <div className='col-sm'>
+                    <Budget />
+                </div>
                         }        
 
                         {
-                            /* Add Remaining component here*/
+                            <div className='col-sm'>
+                            <Remaining/>
+                        </div>
                         }        
 
                         {
-                            /* Add ExpenseTotal component here */
+                             <div className='col-sm'>
+                             <ExpenseTotal/>
+                         </div>
                         }        
                        
                         {
-                            /* Add ExpenseList component here */
+                            <div className='col-sm'>
+                            <ExpenseList/>
+                        </div>
                         }         
 
                         {
-                            /* Add ExpenseItem component here */
+                             <div className='col-sm'>
+                             <ExpenseItem/>
+                         </div>
                         }        
 
                         {
-                            /* Add AllocationForm component here under */
+                            <div className='col-sm'>
+                            <AllocationForm/>
+                        </div>
                         }        
 
                 </div>
